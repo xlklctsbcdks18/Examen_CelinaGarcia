da676282e7097bff62fea630823b5304cffbc6f9
Celina García <mcelina_21@hotmail.com>
2015-10-30 14:58:32 -0600

    Agregue proyecto unity

---
+++ Examen/Assets/Script/Cubo_azul.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubo_azul : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject,3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Examen/Assets/Script/EmisorAmarillo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmisorAmarillo : MonoBehaviour {
+	public GameObject objetoPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject,1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float pos;
+		pos = Random.Range(-5,5);
+		Vector3 newPos = new Vector3(this.transform.position.x+pos,this.transform.position.y, this.transform.position.z);
+		Instantiate(objetoPrefab,newPos,Quaternion.identity);
+	}
+}
